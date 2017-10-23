1c87cf8672c7fcd99bb00dc6d7b2499f1f39b835
Justin Holt <justinvholt@gmail.com>
2017-10-23 07:42:44 +0000

    geocode

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,8 @@ class Place < ApplicationRecord
     validates :name, presence: true, length: { maximum: 40, minimum: 3, too_short: "Awfully short name for a restaurant, no?", too_long: "Is this the name of a restaurant or a run-on sentence?" }
     validates :address, presence: true
     validates :description, presence: true
+
+    geocoded_by :address
+    after_validation :geocode
+
 end
