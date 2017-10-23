b921837ec0316890a80730cbeabef173b1ea3d76
Justin Holt <justinvholt@gmail.com>
2017-10-23 21:03:49 +0000

    comment section

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
     belongs_to :user
+    has_many :comments
+    
     validates :name, presence: true, length: { maximum: 40, minimum: 3, too_short: "Awfully short name for a restaurant, no?", too_long: "Is this the name of a restaurant or a run-on sentence?" }
     validates :address, presence: true
     validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023061723) do
+ActiveRecord::Schema.define(version: 20171023202349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 20171023061723) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.string   "rating"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_comments_on_place_id", using: :btree
+    t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
