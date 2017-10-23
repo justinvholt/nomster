06fa81777d405302aa3d271175f16de5780bd5f9
Justin Holt <justinvholt@gmail.com>
2017-10-23 03:50:07 +0000

    validating user input

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :delete]
+  
+  # @admin = Place.where(:user_id => 1)
 
   def index
     @places = Place.all.paginate(page: params[:page], per_page: 5) 
@@ -10,9 +12,12 @@ class PlacesController < ApplicationController
   end
   
   def create
-    current_user.places.create(place_params)
-    
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def show
@@ -21,16 +26,33 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      render file: Rails.root.join('public', 'not_found.html'), status: :not_found, layout: false
+    end
   end
   
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      render file: Rails.root.join('public', 'not_found.html'), status: :not_found, layout: false
+    end
+    
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      render file: Rails.root.join('public', 'not_found.html'), status: :not_found, layout: false
+    end
+
     @place.destroy
     redirect_to root_path
   end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ApplicationRecord
     belongs_to :user
+    validates :name, presence: true, length: { maximum: 40, minimum: 3, too_short: "Awfully short name for a restaurant, no?", too_long: "Is this the name of a restaurant or a run-on sentence?" }
+    validates :address, presence: true
+    validates :description, presence: true
 end
